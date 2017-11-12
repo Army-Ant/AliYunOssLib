3f8ea9f5dffe6caf13ddde6f7483657abcaff2ed
Jason-Zhao-Jie <261343578@qq.com>
2017-11-12 15:20:49 +0800

    Filling the C++ code

---
+++ AliYunOssLib_Cpp/inc/constant.h
@@ -0,0 +1,71 @@
+#ifndef CONSTANT_H_2017_11_12
+#define CONSTANT_H_2017_11_12
+
+namespace ArmyAnt {
+
+namespace AliYunTools {
+
+namespace RegionEndpoint {
+
+const char*const oss_cn_hangzhou = "oss-cn-hangzhou.aliyuncs.com";			// 华东 1
+const char*const oss_cn_shanghai = "oss-cn-shanghai.aliyuncs.com";			// 华东 2
+const char*const oss_cn_qingdao = "oss-cn-qingdao.aliyuncs.com";			// 华北 1
+const char*const oss_cn_beijing = "oss-cn-beijing.aliyuncs.com";			// 华北 2
+const char*const oss_cn_zhangjiakou = "oss-cn-zhangjiakou.aliyuncs.com";	// 华北 3
+const char*const oss_cn_huhehaote = "oss-cn-huhehaote.aliyuncs.com";		// 华北 5
+const char*const oss_cn_shenzhen = "oss-cn-shenzhen.aliyuncs.com";			// 华南 1
+const char*const oss_cn_hongkong = "oss-cn-hongkong.aliyuncs.com";			// 香港
+const char*const oss_us_west_1 = "oss-us-west-1.aliyuncs.com";				// 美国西部 1 （硅谷）
+const char*const oss_us_east_1 = "oss-us-east-1.aliyuncs.com";				// 美国东部 1 （弗吉尼亚）
+const char*const oss_ap_southeast_1 = "oss-ap-southeast-1.aliyuncs.com";	// 亚太东南 1 （新加坡）
+const char*const oss_ap_southeast_2 = "oss-ap-southeast-2.aliyuncs.com";	// 亚太东南 2 （悉尼）
+const char*const oss_ap_southeast_3 = "oss-ap-southeast-3.aliyuncs.com";	// 亚太东南 3 （吉隆坡）
+const char*const oss_ap_northeast_1 = "oss-ap-northeast-1.aliyuncs.com";	// 亚太东北 1 （日本）
+const char*const oss_eu_central_1 = "oss-eu-central-1.aliyuncs.com";		// 欧洲中部 1 （法兰克福）
+const char*const oss_me_east_1 = "oss-me-east-1.aliyuncs.com";				// 中东东部 1 （迪拜）
+
+const char*const oss_cn_hangzhou_ecs = "oss-cn-hangzhou-internal.aliyuncs.com";
+const char*const oss_cn_shanghai_ecs = "oss-cn-shanghai-internal.aliyuncs.com";
+const char*const oss_cn_qingdao_ecs = "oss-cn-qingdao-internal.aliyuncs.com";
+const char*const oss_cn_beijing_ecs = "oss-cn-beijing-internal.aliyuncs.com";
+const char*const oss_cn_zhangjiakou_ecs = "oss-cn-zhangjiakou-internal.aliyuncs.com";
+const char*const oss_cn_huhehaote_ecs = "oss-cn-huhehaote-internal.aliyuncs.com";
+const char*const oss_cn_shenzhen_ecs = "oss-cn-shenzhen-internal.aliyuncs.com";
+const char*const oss_cn_hongkong_ecs = "oss-cn-hongkong-internal.aliyuncs.com";
+const char*const oss_us_west_1_ecs = "oss-us-west-1-internal.aliyuncs.com";
+const char*const oss_us_east_1_ecs = "oss-us-east-1-internal.aliyuncs.com";
+const char*const oss_ap_southeast_1_ecs = "oss-ap-southeast-1-internal.aliyuncs.com";
+const char*const oss_ap_southeast_2_ecs = "oss-ap-southeast-2-internal.aliyuncs.com";
+const char*const oss_ap_southeast_3_ecs = "oss-ap-southeast-3-internal.aliyuncs.com";
+const char*const oss_ap_northeast_1_ecs = "oss-ap-northeast-1-internal.aliyuncs.com";
+const char*const oss_eu_central_1_ecs = "oss-eu-central-1-internal.aliyuncs.com";
+const char*const oss_me_east_1_ecs = "oss-me-east-1-internal.aliyuncs.com";
+
+const char*const oss_cn_hangzhou_vpc = "oss-cn-hangzhou-internal.aliyuncs.com";
+const char*const oss_cn_shanghai_vpc = "oss-cn-shanghai-internal.aliyuncs.com";
+const char*const oss_cn_qingdao_vpc = "oss-cn-qingdao-internal.aliyuncs.com";
+const char*const oss_cn_beijing_vpc = "oss-cn-beijing-internal.aliyuncs.com";
+const char*const oss_cn_zhangjiakou_vpc = "oss-cn-zhangjiakou-internal.aliyuncs.com";
+const char*const oss_cn_huhehaote_vpc = "oss-cn-huhehaote-internal.aliyuncs.com";
+const char*const oss_cn_shenzhen_vpc = "oss-cn-shenzhen-internal.aliyuncs.com";
+const char*const oss_cn_hongkong_vpc = "oss-cn-hongkong-internal.aliyuncs.com";
+const char*const oss_us_west_1_vpc = "oss-us-west-1-internal.aliyuncs.com";
+const char*const oss_us_east_1_vpc = "oss-us-east-1-internal.aliyuncs.com";
+const char*const oss_ap_southeast_1_vpc = "oss-ap-southeast-1-internal.aliyuncs.com";
+const char*const oss_ap_southeast_2_vpc = "oss-ap-southeast-2-internal.aliyuncs.com";
+const char*const oss_ap_southeast_3_vpc = "oss-ap-southeast-3-internal.aliyuncs.com";
+const char*const oss_ap_northeast_1_vpc = "oss-ap-northeast-1-internal.aliyuncs.com";
+const char*const oss_eu_central_1_vpc = "oss-eu-central-1-internal.aliyuncs.com";
+const char*const oss_me_east_1_vpc = "oss-me-east-1-internal.aliyuncs.com";
+
+const char*const oss_cn_hzfinance = "oss-cn-hzfinance-internal.aliyuncs.com";			// 华东 1
+const char*const oss_cn_shenzhen_finance_1 = "oss-cn-shenzhen-finance-1-internal.aliyuncs.com";			// 华南 1
+const char*const oss_cn_shanghai_finance_1 = "oss-cn-shanghai-finance-1-internal.aliyuncs.com";			// 华东 2
+
+} // namespace RegionEndpoint
+
+} // namespace AliYunTools
+
+} // namespace ArmyAnt
+
+#endif // CONSTANT_H_2017_11_12
---
+++ AliYunOssLib_Cpp/inc/oss_request_options.h
@@ -0,0 +1,26 @@
+#ifndef OSS_REQUEST_OPTIONS_H_2017_11_12
+#define OSS_REQUEST_OPTIONS_H_2017_11_12
+
+namespace ArmyAnt {
+
+namespace AliYunTools {
+
+namespace Oss {
+
+class RequestOptions {
+public:
+	RequestOptions();
+	~RequestOptions();
+
+public:
+
+
+}; // class RequestOptions
+
+} // namespace Oss
+
+} // namespace AliYunTools
+
+} // namespace ArmyAnt
+
+#endif // OSS_REQUEST_OPTIONS_H_2017_11_12
